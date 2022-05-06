4a3177a61a54a56e39402e9f90a06a83d3e00f52
redguy212 <youcanemailandrewmiller@gmail.com>
2022-05-06 17:56:50 +1000

    change from bad to good

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/qajUDQXuZgk?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
