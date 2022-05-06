45c33eb172803001a4fa7e321aacbce3f9fa1f89
redguy212 <youcanemailandrewmiller@gmail.com>
2022-05-06 17:50:27 +1000

    change from bad to good

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rohan is bad at surf
+          Rohan is good at surf
         </p>
         <a
           className="App-link"
