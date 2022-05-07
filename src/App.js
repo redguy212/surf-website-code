0922bb2eddb618be25986bde1c48501b84e7241d
Bryce <bryce.harlum@gmail.com>
2022-05-07 16:05:56 +1000

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AppRoutes from "./routes/routes";
 import { BrowserRouter } from "react-router-dom";
 
 
+// test
 function App() {
   return (
     <div data-testid="app-container" className="App">
